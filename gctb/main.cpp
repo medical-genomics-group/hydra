58933fea4421aa6f6079540b83e73bf315e260ed
Jian Zeng <jzengr@gmail.com>
2017-11-22 20:13:14 +1000

    Initial Commit

---
+++ gctb/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  gctb
+//
+//  Created by Jian Zeng on 22/11/2017.
+//  Copyright © 2017 Jian Zeng. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
