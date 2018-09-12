686dea8a73bcb6e495d7e4f22e6f08ac32ee0481
Daniel Trejo <daniel.trejobanos@unil.ch>
2018-09-12 13:21:28 +0200

    more changes

--- src/test.sh
+++ src/test.sh
@@ -11,6 +11,6 @@ out=$dataset.ref
 
 echo Running reference solution on dataset $dataset. Output in $out
 
-./gctb --bayes bayes --bfile ../test/data/$dataset --pheno ../test/data/test.phen > $out
+./brr --bayes bayes --bfile ../test/data/$dataset --pheno ../test/data/test.phen > $out
 
 echo done
