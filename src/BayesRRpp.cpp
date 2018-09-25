78e8cd3f0797cdf561a10d2e0befef844444bd1b
Sean Harmer <sean.harmer@kdab.com>
2018-09-25 15:45:45 +0100

    Coding style cleanup
    Start to make the code a little more readable to get an idea of
    what is being done.

--- src/BayesRRpp.cpp
+++ src/BayesRRpp.cpp
@@ -7,8 +7,6 @@
 
 #include "BayesRRpp.h"
 #include "data.hpp"
-#include <Eigen/Core>
-#include <random>
 #include "distributions_boost.hpp"
 #include "concurrentqueue.h"
 
@@ -40,219 +38,199 @@ int BayesRRpp::runGibbs()
     unsigned int N(data.numKeptInds);
     std::vector<int> markerI;
     int marker;
-    int K(cva.size()+1);
+    int K(int(cva.size()) + 1);
     VectorXd components(M);
     VectorXf normedSnpData(data.numKeptInds);
 
-    flag=0;
+    flag = 0;
+    std::cout << "Running Gibbs sampling";
 
-
-    std::cout<<"Running Gibbs sampling";
-
-              // Compute the SNP data length in bytes
-     size_t snpLenByt = (data.numInds % 4) ? data.numInds / 4 + 1 : data.numInds / 4;
-
-     omp_set_nested(1); // 1 - enables nested parallelism; 0 - disables nested parallelism.
+    // Compute the SNP data length in bytes
+    size_t snpLenByt = (data.numInds % 4) ? data.numInds / 4 + 1 : data.numInds / 4;
+    omp_set_nested(1); // 1 - enables nested parallelism; 0 - disables nested parallelism.
 
 #pragma omp parallel  shared(flag,q,M,N)
-{
+    {
 #pragma omp sections
-{
+        {
+            {
+                // Mean and residual variables
+                double mu; // mean or intercept
+                double sigmaG; //genetic variance
+                double sigmaE; // residuals variance
 
-  {
-              //mean and residual variables
-                 double mu; // mean or intercept
-                 double sigmaG; //genetic variance
-                 double sigmaE; // residuals variance
+                // Component variables
+                VectorXd priorPi(K); // prior probabilities for each component
+                VectorXd pi(K); // mixture probabilities
+                VectorXd cVa(K); //component-specific variance
+                VectorXd logL(K); // log likelihood of component
+                VectorXd muk(K); // mean of k-th component marker effect size
+                VectorXd denom(K-1); // temporal variable for computing the inflation of the effect variance for a given non-zero componnet
+                double num;//storing dot product
+                int m0; // total num ber of markes in model
+                VectorXd v(K); //variable storing the component assignment
+                VectorXd cVaI(K);// inverse of the component variances
 
-                 //component variables
-                 VectorXd priorPi(K); // prior probabilities for each component
-                 VectorXd pi(K); // mixture probabilities
-                 VectorXd cVa(K); //component-specific variance
-                 VectorXd logL(K); // log likelihood of component
-                 VectorXd muk(K); // mean of k-th component marker effect size
-                 VectorXd denom(K-1); // temporal variable for computing the inflation of the effect variance for a given non-zero componnet
-                 double num;//storing dot product
-                 int m0; // total num ber of markes in model
-                 VectorXd v(K); //variable storing the component assignment
-                 VectorXd cVaI(K);// inverse of the component variances
+                // Linear model variables
+                MatrixXd beta(M,1); // effect sizes
+                VectorXd y_tilde(N); // variable containing the adjusted residuals to exclude the effects of a given marker
+                VectorXd epsilon(N); // variable containing the residuals
 
-                 //linear model variables
-                 MatrixXd beta(M,1); // effect sizes
-                 VectorXd y_tilde(N); // variable containing the adjusted residuals to exclude the effects of a given marker
-                 VectorXd epsilon(N); // variable containing the residuals
+                // Sampler variables
+                VectorXd sample(2*M+4+N); // varible containg a sambple of all variables in the model, M marker effects, M component assigned to markers, sigmaE, sigmaG, mu, iteration number and Explained variance
+                std::vector<int> markerI(M);
+                std::iota(markerI.begin(), markerI.end(), 0);
 
-                 //sampler variables
-                 VectorXd sample(2*M+4+N); // varible containg a sambple of all variables in the model, M marker effects, M component assigned to markers, sigmaE, sigmaG, mu, iteration number and Explained variance
-                 std::vector<int> markerI(M);
-                 std::iota(markerI.begin(), markerI.end(), 0);
+                int marker;
+                double acum;
 
+                VectorXd y;
+                VectorXd Cx;
+                priorPi[0] = 0.5;
 
-                 int marker;
-                 double acum;
+                cVa.segment(1, (K - 1)) = cva;
+                priorPi.segment(1, (K - 1)) = priorPi[0] * cVa.segment(1, (K - 1)).array() / cVa.segment(1, (K - 1)).sum();
+                y_tilde.setZero();
+                cVa[0] = 0;
 
-                 VectorXd y;
-                 VectorXd Cx;
-                 priorPi[0]=0.5;
+                cVaI[0] = 0;
+                cVaI.segment(1, (K - 1))=cVa.segment(1, (K - 1)).cwiseInverse();
 
-			     cVa.segment(1,(K-1))=cva;
-			     priorPi.segment(1,(K-1))=priorPi[0]*cVa.segment(1,(K-1)).array()/cVa.segment(1,(K-1)).sum();
-			     y_tilde.setZero();
-			     cVa[0] = 0;
+                beta.setZero();
+                mu = 0;
+                sigmaG = dist.beta_rng(1, 1);
+                pi = priorPi;
 
-                 cVaI[0] = 0;
-                 cVaI.segment(1,(K-1))=cVa.segment(1,(K-1)).cwiseInverse();
+                components.setZero();
+                std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
 
-                 beta.setZero();
-                 mu=0;
-                 sigmaG = dist.beta_rng(1,1);
+                y = (data.y.cast<double>().array() - data.y.cast<double>().mean());
+                y /= sqrt(y.squaredNorm() / (double(N)-1.0));
 
-                 pi = priorPi;
+                epsilon = y;
+                sigmaE = epsilon.squaredNorm() / N * 0.5;
 
-                 components.setZero();
-                 std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
+                // THIS FOR MUST NOT BE PARALLELIZED, its the markov chain
+                for (int iteration = 0; iteration < max_iterations; iteration++) {
+                    if (iteration > 0) {
+                        if (iteration % int(std::ceil(max_iterations / 10)) == 0)
+                            std::cout << "iteration: "<<iteration <<"\n";
+                    }
 
-			     y=(data.y.cast<double>().array()-data.y.cast<double>().mean());
-			     y/=sqrt(y.squaredNorm()/((double)N-1.0));
+                    epsilon = epsilon.array() + mu; // We substract previous value
+                    mu = dist.norm_rng(epsilon.sum() / double(N), sigmaE / double(N)); // Update mu
+                    epsilon = epsilon.array() - mu;// we substract again now epsilon =Y-mu-X*beta
 
-                 epsilon = y;
-                 sigmaE = epsilon.squaredNorm()/N*0.5;
+                    std::random_shuffle(markerI.begin(), markerI.end());
 
-                 //THIS FOR MUST NOT BE PARALLELIZED, its the markov chain
-                 for(int iteration=0; iteration < max_iterations; iteration++){
+                    m0 = 0;
+                    v.setZero();
 
-                   if(iteration>0)
-                     if( iteration % (int)std::ceil(max_iterations/10) ==0)
-                    std::cout << "iteration: "<<iteration <<"\n";
+                    // THIS FOR should not be parallelized, resulting chain is not ergodic, but it may converge to result sometimes if parallelized
+                    for (int j = 0; j < M; j++ ) {
+                        marker = markerI[j];
+                        // TODO mappedZ column should be scaled, that is, column = (column - mean(column)) / sd(column), maybe that can be done in preprocess
+                        Cx = data.mappedZ.col(marker).cast<double>();
 
-			       epsilon= epsilon.array()+mu;//  we substract previous value
-			       mu = dist.norm_rng(epsilon.sum()/(double)N, sigmaE/(double)N); //update mu
-			       epsilon= epsilon.array()-mu;// we substract again now epsilon =Y-mu-X*beta
+                        y_tilde = epsilon.array()+(Cx * beta(marker, 0)).array(); // Now y_tilde = Y-mu-X*beta+ X.col(marker)*beta(marker)_old
 
+                        muk[0] = 0.0; // muk for the zeroth component=0
 
-                   std::random_shuffle(markerI.begin(), markerI.end());
+                        // We compute the denominator in the variance expression to save computations
+                        denom =(double(N) - 1.0) + (sigmaE / sigmaG) * cVaI.segment(1, (K - 1)).array();
+                        // We compute the dot product to save computations
+                        num = (Cx.cwiseProduct(y_tilde)).sum();
+                        // muk for the other components is computed according to equaitons
+                        muk.segment(1, (K - 1)) = num / denom.array();
 
-                   m0=0;
-                   v.setZero();
+                        logL = pi.array().log(); // First component probabilities remain unchanged
 
-                   //THIS FOR should not be parallelized, resulting chain is not ergodic, but it may converge to result sometimes if parallelized
-                   for(int j=0; j < M; j++){
+                        // Update the log likelihood for each component
+                        logL.segment(1, (K - 1)) = logL.segment(1, (K - 1)).array() - 0.5 * ((((sigmaG / sigmaE) * ((double(N) - 1.0))) * cVa.segment(1, (K - 1)).array() + 1).array().log()) + 0.5 * (muk.segment(1, (K - 1)).array() * num) / sigmaE;
 
-                     marker= markerI[j];
-                     //TODO mappedZ column should be scaled, that is, column=(column-mean(column))/sd(column), maybe that can be done in preprocess
-                     Cx=data.mappedZ.col(marker).cast<double>();
+                        double p(dist.beta_rng(1, 1)); // I use beta(1,1) because I cant be bothered in using the std::random or create my own uniform distribution, I will change it later
 
-                     y_tilde= epsilon.array()+(Cx*beta(marker,0)).array();//now y_tilde= Y-mu-X*beta+ X.col(marker)*beta(marker)_old
+                        if (((logL.segment(1, (K - 1)).array() - logL[0]).abs().array() > 700 ).any()) {
+                            acum = 0;
+                        } else {
+                            acum = 1.0 / ((logL.array() - logL[0]).exp().sum());
+                        }
 
-                     muk[0]=0.0;//muk for the zeroth component=0
+                        for (int k = 0; k < K; k++) {
+                            if (p <= acum) {
+                                // If zeroth component
+                                if (k == 0) {
+                                    beta(marker, 0) = 0;
+                                } else {
+                                    beta(marker,0 ) = dist.norm_rng(muk[k], sigmaE / denom[k-1]);
+                                }
+                                v[k] += 1.0;
+                                components[marker] = k;
+                                break;
+                            } else {
+                                // If too big or too small
+                                if (((logL.segment(1, (K - 1)).array() - logL[k + 1]).abs().array() > 700 ).any()) {
+                                    acum += 0;
+                                } else {
+                                    acum += 1.0 / ((logL.array() - logL[k + 1]).exp().sum());
+                                }
+                            }
+                        }
+                        epsilon = y_tilde - (Cx) * beta(marker, 0); // Now epsilon contains Y-mu - X*beta+ X.col(marker)*beta(marker)_old- X.col(marker)*beta(marker)_new
+                    }
 
-			         //we compute the denominator in the variance expression to save computations
-			         denom=(double(N)-1.0)+(sigmaE/sigmaG)*cVaI.segment(1,(K-1)).array();
-			         //we compute the dot product to save computations
-			         num=(Cx.cwiseProduct(y_tilde)).sum();
-			         //muk for the other components is computed according to equaitons
-			         muk.segment(1,(K-1))= num/denom.array();
+                    m0 = int(M) - int(v[0]);
+                    //cout << "inv scaled parameters " << v0G + m0 << "__" << (beta.squaredNorm() * m0 + v0G * s02G) / (v0G + m0);
+                    sigmaG = dist.inv_scaled_chisq_rng(v0G + m0, (beta.squaredNorm() * m0 + v0G * s02G) / (v0G + m0));
+                    sigmaE = dist.inv_scaled_chisq_rng(v0E + N, ((epsilon).squaredNorm() + v0E * s02E) / (v0E + N));
 
-                     logL= pi.array().log();//first component probabilities remain unchanged
+                    // cout << "sigmaE " << sigmaE << "\n";
+                    // cout << "sigmaG " << sigmaG << "\n";
+                    // cout << "mu " << mu << "\n";
 
-			         //update the log likelihood for each component
-			         logL.segment(1,(K-1))=logL.segment(1,(K-1)).array() - 0.5*((((sigmaG/sigmaE)*((double(N)-1.0)))*cVa.segment(1,(K-1)).array() + 1).array().log()) + 0.5*( muk.segment(1,(K-1)).array()*num)/sigmaE;
+                    pi = dist.dirichilet_rng(v.array() + 1.0);
 
-			         double p(dist.beta_rng(1,1));//I use beta(1,1) because I cant be bothered in using the std::random or create my own uniform distribution, I will change it later
+                    if (iteration >= burn_in) {
+                        if (iteration % thinning == 0) {
+                            sample << iteration, mu, beta, sigmaE, sigmaG, components, epsilon;
+                            q.enqueue(sample);
+                        }
+                    }
+                }
 
-                     if(((logL.segment(1,(K-1)).array()-logL[0]).abs().array() >700 ).any() ){
-                      acum=0;
-                     }else{
-                       acum=1.0/((logL.array()-logL[0]).exp().sum());
-                     }
+                std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();
+                auto duration = std::chrono::duration_cast<std::chrono::seconds>(t2 - t1).count();
+                std::cout << "duration: "<< duration << "s\n";
+                flag = 1;
+            }
+#pragma omp section
+            {
+                bool queueFull;
+                unsigned int i;
+                queueFull = 0;
+                std::ofstream outFile;
+                outFile.open(outputFile);
+                VectorXd sampleq(2 * M + 4 + N);
+                IOFormat CommaInitFmt(StreamPrecision, DontAlignCols, ", ", ", ", "", "", "", "");
+                outFile << "iteration," << "mu,";
+                for (unsigned int i = 0; i < M; ++i) {
+                    outFile << "beta[" << (i + 1) << "],";
+                }
+                outFile << "sigmaE," << "sigmaG,";
+                for (unsigned int i = 0; i < M; ++i) {
+                    outFile << "comp[" << (i+1) << "],";
+                }
 
-			         for(int k=0;k<K;k++){
-			           if(p<=acum){
-			             //if zeroth component
-			             if(k==0){
-			               beta(marker,0)=0;
-			             }else{
-			               beta(marker,0)=dist.norm_rng(muk[k],sigmaE/denom[k-1]);
-			             }
-			             v[k]+=1.0;
-			             components[marker]=k;
-			             break;
-			           }else{
-			             //if too big or too small
-			             if(((logL.segment(1,(K-1)).array()-logL[k+1]).abs().array() >700 ).any() ){
-			               acum+=0;
-			             }
-			             else{
-			               acum+=1.0/((logL.array()-logL[k+1]).exp().sum());
-			             }
-			           }
-			         }
-			        epsilon=y_tilde-(Cx)*beta(marker,0);//now epsilon contains Y-mu - X*beta+ X.col(marker)*beta(marker)_old- X.col(marker)*beta(marker)_new
+                outFile << "epsilon[" << (i+1) << "]";
+                outFile<<"\n";
 
-
-                   }
-
-			       m0=M-v[0];
-			       //cout<< "inv scaled parameters "<< v0G+m0 << "__"<<(beta.squaredNorm()*m0+v0G*s02G)/(v0G+m0);
-			       sigmaG=dist.inv_scaled_chisq_rng(v0G+m0,(beta.squaredNorm()*m0+v0G*s02G)/(v0G+m0));
-			       sigmaE=dist.inv_scaled_chisq_rng(v0E+N,((epsilon).squaredNorm()+v0E*s02E)/(v0E+N));
-
-			      // cout<< "sigmaE "<< sigmaE <<"\n";
-			      // cout<< "sigmaG "<< sigmaG <<"\n";
-			      // cout<< "mu "<< mu <<"\n";
-
-                   pi = dist.dirichilet_rng(v.array() + 1.0);
-
-                   if(iteration >= burn_in)
-                   {
-                     if(iteration % thinning == 0){
-                       sample<< iteration,mu,beta,sigmaE,sigmaG,components,epsilon;
-                       q.enqueue(sample);
-                     }
-
-                   }
-
-                 }
-
-                 std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();
-                 auto duration = std::chrono::duration_cast<std::chrono::seconds>( t2 - t1 ).count();
-                 std::cout << "duration: "<<duration << "s\n";
-                 flag=1;
-
-  }
-  #pragma omp section
-  {
-    bool queueFull;
-    unsigned int i;
-    queueFull=0;
-    std::ofstream outFile;
-    outFile.open(outputFile);
-    VectorXd sampleq(2*M+4+N);
-    IOFormat CommaInitFmt(StreamPrecision, DontAlignCols, ", ", ", ", "", "", "", "");
-    outFile<< "iteration,"<<"mu,";
-    for(unsigned int i = 0; i < M; ++i){
-      outFile << "beta[" << (i+1) << "],";
-
+                while(!flag) {
+                    if (q.try_dequeue(sampleq))
+                        outFile << sampleq.transpose().format(CommaInitFmt) << "\n";
+                }
+            }
+        }
     }
-    outFile<<"sigmaE,"<<"sigmaG,";
-    for(unsigned int i = 0; i < M; ++i){
-      outFile << "comp[" << (i+1) << "],";
-    }
-    for(i = 0; i < (N-1); ++i){
-      outFile << "epsilon[" << (i+1) << "],";
-    }
-    outFile << "epsilon[" << (i+1) << "]";
-    outFile<<"\n";
 
-    while(!flag ){
-      if(q.try_dequeue(sampleq))
-        outFile<< sampleq.transpose().format(CommaInitFmt) << "\n";
-    }
-  }
-
-  }
-  }
-
- return 0;
-
+    return 0;
 }
--- src/BayesRRpp.h
+++ src/BayesRRpp.h
@@ -14,15 +14,12 @@
 
 class BayesRRpp
 {
-    Data            &data;//data matrices
-    Options         &opt;
-    const string    bedFile;//bed file
-    const long      memPageSize;//size of memory
-    const string    outputFile;
-    const int       seed;
-    const int       max_iterations;
-    const int		burn_in;
-    const int       thinning;
+    Data            &data; // Data matrices
+    const string    outputFile = "bayesOutput.csv";
+    const int       seed = 1;
+    const int       max_iterations = 10;
+    const int		burn_in = 5;
+    const int       thinning = 1;
     const double	sigma0 = 0.0001;
     const double	v0E = 0.0001;
     const double    s02E = 0.0001;
@@ -34,7 +31,7 @@ class BayesRRpp
 public:
     BayesRRpp(Data &data, Options &opt, const long memPageSize);
     virtual ~BayesRRpp();
-    int runGibbs(); //where we run Gibbs sampling over the parametrised model
+    int runGibbs(); // Where we run Gibbs sampling over the paramaterised model
 };
 
 #endif /* SRC_BAYESRRM_H_ */
