34bcbf543cbd3416f442db50809dde00bb0bfd6e
Daniel Trejo banos <dtrejoba@dbc-serv04.vital-it.ch>
2018-10-08 10:56:47 +0200

    added more environmental variables to compile using clang

--- src/SET_VITENV.sh
+++ src/SET_VITENV.sh
@@ -1,2 +1,7 @@
 module add UHTS/Analysis/plink/1.90
 module add Development/mpi/openmpi-x86_64
+export LDFLAGS="-Wl,--rpath=/software/lib64/ -Wl,--dynamic-linker=/software/lib64/ld-linux-x86-64.so.2 -L/lib/"
+export CC=clang
+export CXX=clang++
+export MPICXX=clang++
+export MPICC=clang
