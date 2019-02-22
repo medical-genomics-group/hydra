bfc499d74a5fd3ab726aae4b5cdb879618f2d3db
kousathanas <kousathanas.a@gmail.com>
2019-02-22 20:16:59 +0100

    add compression

---
+++ src/compression.h
@@ -0,0 +1,19 @@
+#ifndef COMPRESSION_H
+#define COMPRESSION_H
+
+#include <Eigen/Eigen>
+
+using namespace Eigen;
+
+unsigned long maxCompressedDataSize(const unsigned int numFloats);
+
+unsigned long compressData(const VectorXd &snpData,
+                           unsigned char *outputBuffer,
+                           unsigned long outputSize);
+
+void extractData(unsigned char *compressedData,
+                 unsigned int compressedDataSize,
+                 unsigned char *outputBuffer,
+                 unsigned int outputBufferSize);
+
+#endif // COMPRESSION_H
