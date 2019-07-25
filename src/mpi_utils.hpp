62fe659446935c146408fb5fdc7e4ee027e99971
orliac <etienne.orliac@unil.ch>
2019-07-25 09:17:47 +0200

    minor change in printed error message

--- src/mpi_utils.hpp
+++ src/mpi_utils.hpp
@@ -7,7 +7,7 @@
 // --------------------------------------------------------------------------
 inline void check_mpi(const int error, const int linenumber, const char* filename) {
     if (error != MPI_SUCCESS) {
-        fprintf(stderr, "MPI error %d at line %d of file %s\n", error, linenumber, filename);
+        fprintf(stderr, "*FATAL*: MPI error %d at line %d of file %s\n", error, linenumber, filename);
         MPI_Abort(MPI_COMM_WORLD, 1);
     }
 }
@@ -29,6 +29,7 @@ inline int check_int_overflow(const size_t n, const int linenumber, const char* 
 // -----------------------------------
 inline int check_file_size(const MPI_File fh, const size_t N, const size_t DTSIZE, const int linenumber, const char* filename) {
 
+    //printf("DEBUG  : expected size = %lu x %lu bytes\n", N, DTSIZE);
     size_t     expected_file_size = N * DTSIZE;
 
     MPI_Offset actual_file_size = 0;
