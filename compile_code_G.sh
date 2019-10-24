e4b4fae63be8f73ad91e9625a864357705120a40
orliac <etienne.orliac@unil.ch>
2019-10-24 14:31:54 +0200

    Adding compilation utility scripts for Intel and GCC (_G)

---
+++ compile_code_G.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+module purge
+module load gcc mvapich2 boost eigen
+module list
+
+#LD_LIBRARY_PATH=/usr/lib64:$LD_LIBRARY_PATH
+#echo LD_LIBRARY_PATH = $LD_LIBRARY_PATH
+
+NAM=mpi_gibbs_G
+
+EXE=./src/$NAM
+
+cd ./src
+
+make EXE=$NAM $1 -f Makefile_G || exit 1;
+
+cd ..
+
+if [ ! -f $EXE ]; then
+    echo Fatal: binary $EXE not found!
+    exit
+fi
+
