a57e616a28edcc136d14b3b336523454d47e1124
orliac <etienne.orliac@unil.ch>
2020-03-31 13:32:04 +0200

    Extending to all data representation + sync schemes

--- srun_mixed_representation_processing.sh
+++ srun_mixed_representation_processing.sh
@@ -57,7 +57,7 @@ elif [ $DS == 2 ]; then
     sparsebsn=${dataset}_uint
     NUMINDS=500000
     NUMSNPS=1270420
-    NUMSNPS=33000
+    NUMSNPS=3000
 elif [ $DS == 3 ]; then
     sparsedir=/scratch/orliac/UKBgen/
     sparsebsn=epfl_test_data_sparse
@@ -87,30 +87,26 @@ echo "S         :" $S
 echo "======================================"
 echo
 
-CL=5
+CL=3
 SEED=10
-SR=5
+SR=1
 SM=0
 THIN=1
 SAVE=100
 TOCONV_T=$((($CL - 1) / $THIN))
 echo TOCONV_T $TOCONV_T
 N=1
-TPN=11
+TPN=9
 
-CPT=1 # CPUs per task
+CPT=4 # CPUs per task
 export OMP_NUM_THREADS=$CPT
 export KMP_AFFINITY=verbose
 export KMP_AFFINITY=noverbose
 
-# Set what to run
-run_bed=0;
-run_sparse_sync_dp=1;
-run_sparse_sync_sparse=1;
-run_sparse_sync_bed=1;
-run_mixed_sync_dp=1;
-run_mixed_sync_sparse=1;
-run_mixed_sync_bed=1;
+# Select what to run
+run_bed_sync_dp=1;      run_sparse_sync_dp=1;      run_mixed_sync_dp=1;
+run_bed_sync_sparse=1;  run_sparse_sync_sparse=1;  run_mixed_sync_sparse=1;
+run_bed_sync_bed=1;     run_sparse_sync_bed=1;     run_mixed_sync_bed=1;
 
 
 COV="--covariates $datadir/scaled_covariates.csv"
@@ -121,9 +117,11 @@ BLK=""
 outdir=/home/orliac/DCSR/CTGG/BayesRRcmd/output_tests/
 
 
-if [ $run_bed == 1 ]; then
+### BED data processing
+
+if [ $run_bed_sync_dp == 1 ]; then
     echo; echo
-    echo "@@@ Solution reading from  BED file @@@"
+    echo "@@@ BED + DP @@@"
     echo
     sol=test_mnm2
     cmd="srun -N $N --ntasks-per-node=$TPN --cpus-per-task=$CPT  $EXE --number-individuals $NUMINDS --number-markers $NUMSNPS --mpibayes bayesMPI --bfile $datadir/$dataset --pheno $datadir/${phen}.phen --chain-length $CL --thin $THIN --save $SAVE  --mcmc-out-dir $outdir --mcmc-out-name $sol --seed $SEED --shuf-mark $SM --sync-rate $SR --S $S $COV $BLK"
@@ -131,6 +129,28 @@ if [ $run_bed == 1 ]; then
     $cmd || exit 1
 fi
 
+if [ $run_bed_sync_sparse == 1 ]; then
+    echo; echo
+    echo "@@@ BED + SPARSE @@@"
+    echo
+    sol=test_mnm2
+    cmd="srun -N $N --ntasks-per-node=$TPN --cpus-per-task=$CPT  $EXE --number-individuals $NUMINDS --number-markers $NUMSNPS --mpibayes bayesMPI --bfile $datadir/$dataset --pheno $datadir/${phen}.phen --chain-length $CL --thin $THIN --save $SAVE  --mcmc-out-dir $outdir --mcmc-out-name $sol --seed $SEED --shuf-mark $SM --sync-rate $SR --S $S --sparse-sync $COV $BLK"
+    echo $cmd; echo
+    $cmd || exit 1
+fi
+
+if [ $run_bed_sync_bed == 1 ]; then
+    echo; echo
+    echo "@@@ BED + BED @@@"
+    echo
+    sol=test_mnm2
+    cmd="srun -N $N --ntasks-per-node=$TPN --cpus-per-task=$CPT  $EXE --number-individuals $NUMINDS --number-markers $NUMSNPS --mpibayes bayesMPI --bfile $datadir/$dataset --pheno $datadir/${phen}.phen --chain-length $CL --thin $THIN --save $SAVE  --mcmc-out-dir $outdir --mcmc-out-name $sol --seed $SEED --shuf-mark $SM --sync-rate $SR --S $S --bed-sync $COV $BLK"
+    echo $cmd; echo
+    $cmd || exit 1
+fi
+
+
+### SPARSE data processing
 
 if [ $run_sparse_sync_dp == 1 ]; then
     echo; echo; echo "@@@ SPARSE + DP @@@"
@@ -158,8 +178,7 @@ if [ $run_sparse_sync_bed == 1 ]; then
 fi
 
 
-
-### Mixed-representation processing
+### MIXED-representation data processing
 
 if [ $run_mixed_sync_dp == 1 ]; then
     echo; echo; echo "@@@ MIXED + DP @@@"; echo
@@ -172,6 +191,10 @@ fi
 if [ $run_mixed_sync_sparse == 1 ]; then
     echo; echo; echo "@@@ MIXED + SPARSE @@@"; echo
     sol2=mixed_sync_sparse
+    ampdir=/scratch/orliac/tmp_vtune_profile
+    rm -r $ampdir
+    amp="srun -N $N --ntasks-per-node=$TPN --cpus-per-task=$CPT amplxe-cl –c hotspots –r $ampdir -data-limit=1000 -- "
+    #cmd="$amp  $EXE --number-individuals $NUMINDS --number-markers $NUMSNPS --mpibayes bayesMPI --pheno $sparsedir/${phen}.phen --chain-length $CL --thin $THIN --save $SAVE  --mcmc-out-dir $outdir --mcmc-out-name $sol2 --seed $SEED --shuf-mark $SM --sync-rate $SR --S $S --bfile $datadir/$dataset --sparse-dir $sparsedir  --sparse-basename $sparsebsn --threshold-fnz 0.06 --sparse-sync $COV $BLK"
     cmd="srun -N $N --ntasks-per-node=$TPN --cpus-per-task=$CPT  $EXE --number-individuals $NUMINDS --number-markers $NUMSNPS --mpibayes bayesMPI --pheno $sparsedir/${phen}.phen --chain-length $CL --thin $THIN --save $SAVE  --mcmc-out-dir $outdir --mcmc-out-name $sol2 --seed $SEED --shuf-mark $SM --sync-rate $SR --S $S --bfile $datadir/$dataset --sparse-dir $sparsedir  --sparse-basename $sparsebsn --threshold-fnz 0.06 --sparse-sync $COV $BLK"
     echo $cmd; echo
     $cmd || exit 1
