5fe8a78c4f008fe2b2d8f5113035829fccfe5e07
x4nd3r <anthraxander@gmail.com>
2020-06-15 01:13:44 +0200

    Implement correct row-major mat mul, improve readability

--- src/linpred.cpp
+++ src/linpred.cpp
@@ -16,18 +16,20 @@ LinPred::LinPred(Data &data, Options &opt)
  * post : NxI prediction matrix is stored in pred member variable
  */
 void LinPred::predict_genetic_values(string outfile) {
-    cout << data.Z << std::endl;
-    // perform prediction
+    // initialise prediction matrix
     data.pred.resize(data.Z.rows(), data.predBet.cols());
-    // scatter rows and cols across processes
+
     int nranks, rank;
     MPI_Comm_size(MPI_COMM_WORLD, &nranks);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    // get data dimensions and create arrays
+    // get data dimensions
     int N = data.Z.rows();
     int M = data.Z.cols();
     int I = data.predBet.cols();
-    printf("N = %d, M = %d, I = %d\n", N, M, I);
+    printf("INFO:   N = %d, M = %d, I = %d\n", N, M, I);
+    printf("INFO:   Z has dimension %dx%d\n", (int) data.Z.rows(), (int) data.Z.cols());
+    printf("INFO:   predBet has dimenion %dx%d\n", (int) data.predBet.rows(), (int) data.predBet.cols());
+    printf("INFO:   pred has dimension %dx%d\n", (int) data.pred.rows(), (int) data.pred.cols());
     double a[N*M];
     double b[M*I];
     // map data matrices to arrays for scattering
@@ -35,24 +37,27 @@ void LinPred::predict_genetic_values(string outfile) {
     Map<Matrix<double, Dynamic, Dynamic, RowMajor>>(a, N, M) = data.Z; // LHS row-major format
     Map<MatrixXd>(b, M, I) = data.predBet; // RHS col-major format
     // assume N is divisible by number of processes
+    // TODO: handle case when N is not divisible by number of processes
     int block_rows_a = N / nranks;
-    // get number of elements per block
+    // get number of elements per block in A, and C
     uint elem_per_proc_a = block_rows_a * M;
+    uint block_size = block_rows_a * I;
     double *buff_a = (double *) malloc(sizeof(double) * elem_per_proc_a);
-    double *buff_c = (double *) malloc(sizeof(double) * block_rows_a * I);
-    for (uint i = 0; i < block_rows_a * I; i++) {
+    double *buff_c = (double *) malloc(sizeof(double) * block_size);
+    for (uint i = 0; i < block_size; i++) {
         buff_c[i] = 0.0;
     }
     // scatter unique blocks of A to processors
     printf("INFO: Scattering %d elements of A to each of %d tasks\n", elem_per_proc_a, nranks);
     MPI_Scatter(a, elem_per_proc_a, MPI_DOUBLE, buff_a,
                 elem_per_proc_a, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-    // perform multiplication
+    // perform multiplication (N.B. we must use row-major, as col-major results
+    // in out-of-order blocks when we perform MPIGather.
     //  TODO: parallelise loops
     for (uint i = 0; i < block_rows_a; i++) {
         for (uint j = 0; j < I; j++) {
             for (uint k = 0; k < M; k++) {
-                buff_c[i + block_rows_a * j] += buff_a[i + block_rows_a * k] * b[j * M + k];
+                buff_c[i * I + j] += buff_a[i * M + k] * b[j * M + k];
             }
         }
     }
@@ -61,17 +66,18 @@ void LinPred::predict_genetic_values(string outfile) {
     if (rank == 0) {
         c = (double *) malloc(sizeof(double) * N * I);
     }
-    MPI_Gather(buff_c, block_rows_a * I, MPI_DOUBLE, c,
-                block_rows_a * I, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Gather(buff_c, block_size, MPI_DOUBLE, c,
+                block_size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
     MPI_Barrier(MPI_COMM_WORLD);
+    
     // map c array back to MatrixXd
     if (rank == 0) {
         data.pred = Map<Matrix<double, Dynamic, Dynamic, RowMajor>>(c, N, I);
+        // write prediction matrix to disk
+        // TODO: refactor to a writer function
+        ofstream file(outfile.c_str());
+        file << data.pred.format(csvFormat) << std::endl;
+        file.flush();
+        cout << "Predictions written to disk" << std::endl;
     }
-    // write prediction matrix to disk
-    // TODO: refactor to a writer function
-    ofstream file(outfile.c_str());
-    file << data.pred.format(csvFormat) << std::endl;
-    file.flush();
-    cout << "Predictions written to disk" << std::endl;
 }
