48598a5033c694baf5d13714d224d4b2d494d550
orliac <etienne.orliac@unil.ch>
2020-06-17 14:05:57 +0200

    Update global test utility scripts

--- run_all_bayes_tests.sh
+++ run_all_bayes_tests.sh
@@ -6,12 +6,17 @@ source ./compile_code.sh
 
 export OMP_NUM_THREADS=4
 
-NT=4
+NT=1
 
 export SRUN_OPTS="-n $NT -c $OMP_NUM_THREADS"
 
-sh srun_bayes_test.sh --bayes bayesMPI
+echo; echo "@@@@@@ BAYES R"
+#sh srun_bayes_test.sh --bayes bayesMPI
 
-sh srun_bayes_test.sh --bayes bayesFHMPI
 
-#sh srun_bayes_test.sh --bayes bayesWMPI
+echo; echo "@@@@@@ BAYES FH"
+#sh srun_bayes_test.sh --bayes bayesFHMPI
+
+
+echo; echo "@@@@@@ BAYES W"
+sh srun_temp_bayesW_test.sh --bayes bayesWMPI
