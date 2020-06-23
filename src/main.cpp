e363e51f63ba192161b86400aebae714f268d170
x4nd3r <anthraxander@gmail.com>
2020-06-23 11:22:50 +0200

    Broadcast matrix dimensions from root process

--- src/main.cpp
+++ src/main.cpp
@@ -171,16 +171,23 @@ int main(int argc, const char * argv[]) {
         // AH: prediction
         } else if (opt.predict) {
             // configure data object
+            uint N, M, I;
             if (rank == 0) {
                 data.readBimFile(opt.bimFile);
                 data.readFamFile(opt.famFile);
                 data.readBedFile_noMPI(opt.bedFile);
                 data.read_train_data(opt.trainBim, opt.betLong, opt.betIterations);
+                N = data.Z.rows();
+                M = data.Z.cols();
+                I = data.predBet.cols();
             }
+            MPI_Bcast(&N, 1, MPI_INT, 0, MPI_COMM_WORLD);
+            MPI_Bcast(&M, 1, MPI_INT, 0, MPI_COMM_WORLD);
+            MPI_Bcast(&I, 1, MPI_INT, 0, MPI_COMM_WORLD);
             MPI_Barrier(MPI_COMM_WORLD);
             // perform prediction
             LinPred predictor(data, opt);
-            predictor.predict_genetic_values(data.Z.rows(), data.Z.cols(), data.predBet.cols(), opt.predOutDir + "/" + opt.predOutName);
+            predictor.predict_genetic_values(N, M, I, opt.predOutDir + "/" + opt.predOutName);
         }
         else {
             throw(" Error: Wrong analysis requested: " + opt.analysisType + " + " + opt.bayesType);
