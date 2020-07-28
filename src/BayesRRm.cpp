7fbae63e9715127908d4cf0bceb0dd053161e4c8
daniel-trejobanos <daniel.trejobanos@unil.ch>
2020-07-28 21:28:41 +0200

    corrected small bug with the degrees of freedom of the c_slab

--- src/BayesRRm.cpp
+++ src/BayesRRm.cpp
@@ -1150,7 +1150,7 @@ int BayesRRm::runMpiGibbs() {
                 // FHDT sample hyper parameters
                 hypTau    = dist.inv_gamma_rate_rng(0.5 + 0.5 * v0t, I_TAU0SQ + 1.0 / tau);
                 tau       = dist.inv_gamma_rate_rng(0.5 * (m0[i] + v0t), v0t / hypTau + (0.5 * scaledBSQN));
-                c_slab[i] = dist.inv_scaled_chisq_rng(v0c + (double)m0[i], (beta_squaredNorm[i] * (double)m0[i] + v0c * s02G) / (v0c + (double)m0[i]));
+                c_slab[i] = dist.inv_scaled_chisq_rng(v0c + (double)m0[i], (beta_squaredNorm[i] * (double)m0[i] + v0c * s02c) / (v0c + (double)m0[i]));
                 sigmaG[i] = beta_squaredNorm[i];
                 
                 //DT scaling for ishwaran rao
