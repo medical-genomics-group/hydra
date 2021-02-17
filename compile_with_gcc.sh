79684f1f8f7d4b72e0a2e5e405e45f2f3a51a260
orliac <etienne.orliac@unil.ch>
2021-02-17 11:54:30 +0100

    moved guess for hydra_root above module loads

--- compile_with_gcc.sh
+++ compile_with_gcc.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
+set -e
+
+# before loading modules!!!
+export HYDRA_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+echo HYDRA_ROOT = $HYDRA_ROOT
+[[ "$HYDRA_ROOT" == *hydra ]] || (echo "HYDRA_ROOT ($HYDRA_ROOT) expected to end with \"hydra\"" && exit 1)
+
 module purge
+module load gcc/8.3.0 mvapich2 boost #eigen
+module list
 
-module load gcc mvapich2 boost eigen
+# Local install of eigen to be able to use gcc/8.3.0
+export EIGEN_ROOT=/home/orliac/inc/Eigen-3.3.7
 
-make $1
+cd $HYDRA_ROOT
+HYDRA_EXE=hydra_g
+EXEC=$HYDRA_EXE make -j8 $1 || exit 1
+export HYDRA_EXE=$HYDRA_EXE
+cd -
--- compile_with_intel.sh
+++ compile_with_intel.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
+set -e
+
+# before loading modules!!!
+export HYDRA_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+echo HYDRA_ROOT = $HYDRA_ROOT
+[[ "$HYDRA_ROOT" == *hydra ]] || (echo "HYDRA_ROOT ($HYDRA_ROOT) expected to end with \"hydra\"" && exit 1)
+
 module purge
-
 module load intel intel-mpi boost eigen
+module list
 
-make $1
+cd $HYDRA_ROOT
+HYDRA_EXE=hydra_i
+EXEC=$HYDRA_EXE make -j8 $1 || exit 1
+export HYDRA_EXE=$HYDRA_EXE
+cd -
