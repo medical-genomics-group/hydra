d09dec2b494ef03da453e3869f955c02897d9dae
orliac <etienne.orliac@unil.ch>
2021-02-17 17:30:40 +0100

    builtin_align + openmp simd + godbolt check

--- src/dense.hpp
+++ src/dense.hpp
@@ -1,37 +1,61 @@
 #ifndef HYDRA_DENSE_H
 #define HYDRA_DENSE_H
 
+#include <cstdio>
 
-// Add an offset to each element of the input array
-void offset_array(double* __restrict__ array,
-                  const double         offset,
-                  const int            N);
+inline
+void offset_array(double* __restrict__ in,
+                  const double OFFSET,
+                  const int SIZE) {
+
+    in = (double*)__builtin_assume_aligned(in, 64);
+
+#ifdef _OPENMP
+#pragma omp parallel for simd aligned(in:64)
+#endif
+    for (int i=0; i<SIZE; i++)
+        in[i] += OFFSET;
+}
+
+inline
+void set_array(double* __restrict__ in,
+               const double VALUE,
+               const int SIZE) {
+    in = (double*) __builtin_assume_aligned(in, 64);
+#ifdef _OPENMP
+#pragma omp parallel for simd aligned(in:64)
+#endif
+    for (int i=0; i<SIZE; i++)
+        in[i] = VALUE;
+}
+
+inline
+void copy_array(double*       __restrict__ dst,
+                const double* __restrict__ src,
+                const int SIZE) {
+    dst = (double*) __builtin_assume_aligned(dst, 64);
+    src = (double*) __builtin_assume_aligned(src, 64);
+#ifdef _OPENMP
+#pragma omp parallel for simd aligned(dst,src:64)
+#endif
+    for (int i=0; i<SIZE; i++)
+        dst[i] = src[i];
+}
 
 
-void set_array(double* __restrict__ array,
-               const double         val,
-               const int            N);
+double sum_array_elements(const double* __restrict__ in,
+                          const int N);
+double sum_array_elements(const long double* __restrict__ in,
+                          const int N);
 
-
-void copy_array(double*       __restrict__ dest,
-                const double* __restrict__ source,
-                const int N);
-
-
-double sum_array_elements(const      double* __restrict__ array, const int N);
-double sum_array_elements(const long double* __restrict__ array, const int N);
-
+void add_arrays(double*       __restrict__ pout,
+                const double* __restrict__ pin1,
+                const double* __restrict__ pin2,
+                const int SIZE);
 
 void add_arrays(double*       __restrict__ out,
                 const double* __restrict__ in1,
-                const double* __restrict__ in2,
                 const int N);
-
-
-void add_arrays(double*       __restrict__ out,
-                const double* __restrict__ in1,
-                const int N);
-
 
 void center_and_scale(double* __restrict__ vec,
                       const int            N);
