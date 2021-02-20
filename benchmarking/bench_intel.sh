c2b08f183e5b032a9cc739cee971657221ba886e
orliac <etienne.orliac@unil.ch>
2021-02-21 00:14:06 +0100

    Adjusting the tests

--- benchmarking/bench_intel.sh
+++ benchmarking/bench_intel.sh
@@ -3,7 +3,10 @@
 set -e
 #set -x
 
-source ../compile_with_intel.sh $1
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+echo DIR = $DIR
+
+source $DIR/../compile_with_intel.sh $1
 
 echo HYDRA_ROOT = $HYDRA_ROOT
 echo HYDRA_EXE  = $HYDRA_EXE
@@ -14,23 +17,23 @@ echo BENCH_DIR = $BENCH_DIR
 
 OUT_DIR=/scratch/orliac/bench_hydra
 
-NTASKS=2
-NTHREADS_PER_TASK=1
+NTASKS=6
+NTHREADS_PER_TASK=6
 
 export OMP_NUM_THREADS=$NTHREADS_PER_TASK
 
-CMD_BASE="srun -p build -n $NTASKS --cpus-per-task $NTHREADS_PER_TASK -t 00:10:00 --mem=0 --cpu-bind=verbose,sockets \
+CMD_BASE="srun -p build -n $NTASKS --cpus-per-task $NTHREADS_PER_TASK -t 00:10:00 --mem=0 --cpu-bind=verbose \
     $HYDRA_ROOT/bin/$HYDRA_EXE \
-    --pheno $BENCH_DIR/test.phen \
+    --pheno $BENCH_DIR/test1.phen \
     --mcmc-out-dir $OUT_DIR \
     --mcmc-out-name bench_hydra_epfl_intel \
     --mpibayes bayesMPI \
-    --shuf-mark 0 \
+    --shuf-mark 1 \
     --seed 123 \
-    --number-markers 200 \
+    --number-markers 10000 \
     --number-individuals 500000 \
     --verbosity 0 \
-    --chain-length 3"
+    --chain-length 24"
 
 # Add groups & mixtures
 CMD_GROUPS="--groupIndexFile $BENCH_DIR/test.gri_hydra --groupMixtureFile $BENCH_DIR/test.grm_hydra"
